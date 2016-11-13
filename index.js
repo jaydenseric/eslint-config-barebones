d67c1fe5ebd7eb4fa90fae09cc269686c00e8f38
Jayden Seric <Jayden.Seric@cheproximity.com.au>
2016-11-14 10:40:30 +1100

    Minor config formatting improvements.

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ module.exports = {
   extends: 'standard',
   rules: {
     'arrow-parens': [2, 'as-needed'],
-    'no-console': [2, {
-      'allow': ['warn', 'error']
-    }],
-    'no-new': 1,
-    'valid-jsdoc': [2, {
-      requireReturn: false,
-      prefer: {
-        return: 'returns'
+    'no-console': [
+      2,
+      {
+        allow: ['warn', 'error']
       }
-    }]
+    ],
+    'no-new': 1,
+    'valid-jsdoc': [
+      2,
+      {
+        requireReturn: false,
+        prefer: {
+          return: 'returns'
+        }
+      }
+    ]
   }
 }
